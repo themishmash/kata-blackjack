8a92294a280826bd456af984d2c17a7a17755a30
Michelle <wong.mich.c@gmail.com>
2020-04-01 11:20:34 +1100

    first commit

---
+++ BlackJackKata/Card.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+
+namespace blackjack
+{
+    public class Card
+    {
+      //properties
+      //enum suits
+
+      // public enum Suits
+      // {
+      //     Clubs,
+      //     Diamonds,
+      //     Hearts,
+      //     Spades
+      // }
+      //value numerical
+      public int Value = 1;
+      public string Suit = "hearts";
+
+      public Card(int value)
+      {
+          Value = value;
+         
+      }
+      
+      //constructor to create individual card with 
+    }
+}
